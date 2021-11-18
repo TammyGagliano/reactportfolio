8e0872786dff40e9cb1d1f97c375d4985b5debcb
Tammy Gagliano <tracsd@gmail.com>
2021-11-18 11:29:25 -0600

    update about and footer pages

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import "./style.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>
+        Tammy Gagliano has about nine years’ experience in the finance industry
+        mixed with about ten years of IT experience. She worked as a loan
+        processor while completing her undergraduate degree in Psychology and
+        Sociology at the University of Alabama at Birmingham and minored in
+        Spanish. Tammy obtained an Associates’ degree in Web Technologies and
+        joined the BBVA Web Team in 2016 as a Front-End Developer. In this role,
+        she worked as both a developer and a project manager. She trained
+        content authors in Adobe Experience Manager (AEM) content management
+        process and self-authoring tools, while working directly with lines of
+        businesses, legal and compliance, copy writers and UI/UX teams to create
+        public web landing pages that focus on improving the customers
+        experience through banking, web journey and customer conversions.Tammy
+        joined the Campaign Execution team in 2019 and has been responsible for
+        end to end development and delivery of Digital Marketing Campaigns and
+        Customer Journeys.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a
+        href="https://www.linkedin.com/in/tammygagliano/"
+        target="_blank"
+        alt="LinkedIn Profile"
+      >
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a
+        href="https://github.com/TammyGagliano"
+        target="_blank"
+        alt="GitHub Profile"
+      >
+        <i class="fab fa-github"></i>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
