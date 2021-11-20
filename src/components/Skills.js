d5e5e76cfb1b71a80cff959cf9a18461f8b5be56
Tammy Gagliano <tracsd@gmail.com>
2021-11-20 14:33:06 -0600

    skills update and css update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,42 +1,33 @@
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import "./style.css";
 
 const Skills = () => {
   return (
-    <div className="Portfolio">
-      <h2>Programming Languages &amp; Tools</h2>
-      <ul>
-        <li>HTML5
-        </li>
-        <li>
-          <i class="fab fa-css3-alt"></i>
-        </li>
-        <li>
-          <i class="fab fa-js-square"></i>
-        </li>
-        <li>
-          <i class="fab fa-wordpress"></i>
-        </li>
-        <li>
-        <FontAwesomeIcon icon={["fab", "github"]} />
-        </li>
-        <li>
-          <i class="fab fa-adobe"></i>
-        </li>
-        <li>
-          <i class="fab fa-bootstrap"></i>
-        </li>
-        <li>
-          <i class="fab fa-react"></i>
-        </li>
-        <li>
-          <i class="fab fa-node-js"></i>
-        </li>
-      </ul>
+  <div className="skills">
+  <h1>Skills</h1>
+  <div className="skillscontent">
+    <div>
+      <strong>Languages:</strong>
+      <li>CSS3</li>
+      <li>HTML5</li>
+      <li>Node.js</li>
+      <li>JavaScript</li>
     </div>
-  );
+    <div>
+      <strong>Libraries:</strong>
+      <li>Bootstrap</li>
+      <li>Express.js</li>
+      <li>Material-ui</li>
+      <li>React.js</li>
+    </div>
+    <div>
+      <strong>Tools:</strong>
+      <li>Eslint</li>
+      <li>Webpack</li>
+      <li>Chrome Dev</li>
+      <li>Git & Github</li>
+    </div>
+  </div>
+</div>
+)
 };
-
 export default Skills;
